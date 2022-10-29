13175e74da8b5af8e172bd8e60010e1311efe5c2
IvanGrande <Ivan.enriquez.grande@gmail.com>
2022-10-29 12:55:02 -0500

    Correcciones de rutas

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -4,7 +4,7 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "xsaiodoiewhoesicc"
-    app.config['UPLOAD_PATH'] = "D:\\Programas\\Python\\geovis\\src\\website\\uploads\\test1"
+    app.config['UPLOAD_PATH'] = ".\\website\\uploads\\test1"
     app.config["ALLOWEWD_EXTENSIONS"] = ["SHP", "CPG", "DBF", "SHX", "SBN", "SBX", "XML"]
     
     from .views import views
